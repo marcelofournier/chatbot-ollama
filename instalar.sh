2e4d1505f195d3b44512699d52e8b8c710bd444d
Marcelo Fournier <marcelo.fournier@hotmail.com>
2024-04-15 14:11:41 -0400

    Add files via upload

---
+++ instalar.sh
@@ -0,0 +1,47 @@
+clear
+echo "Preparando tudo...."
+#sudo apt update && sudo apt upgrade -y
+#sudo apt autoremove && sudo apt autoclean
+#sudo apt install python3 python3-pip
+#sudo pip3 install virtualenv
+#virtualenv --version
+echo
+echo "Instalando as bibliotecas..."
+
+# Captura o nome do diretório atual
+current_dir=$(basename "$PWD")
+echo "Diretório atual: $current_dir"
+
+# Retrocede um diretório
+cd ..
+
+# Cria o ambiente virtual python
+virtualenv chatbot-ollama
+#cd chatbot-ollama
+
+# Retorna ao diretório original
+echo "Retornando ao diretório: $current_dir"
+cd "$current_dir"
+source bin/activate
+
+
+pip install tk
+pip install pillow
+pip install ollama
+pip install gtts
+pip install playsound
+echo
+echo "Instalando o Ollama..."
+
+# Verifica se o LLM Ollama está instalado
+if command -v ollama &>/dev/null; then
+    echo "LLM Ollama já está instalado."
+else
+    # Instalação do LLM Ollama
+    echo "LLM Ollama não está instalado. Instalando..."
+    curl -fsSL https://ollama.com/install.sh | sh
+fi
+
+ollama pull llamma2
+ollama list
+python3 chatbot.py config.txt
